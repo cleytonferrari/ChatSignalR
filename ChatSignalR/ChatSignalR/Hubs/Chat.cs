285bb0f2f6d35929944431785c9c02f5a927e032
Cleyton Ferrari <cleyton@w7br.com>
2012-06-10 21:50:13 -0400

    Projeto Inicial

---
+++ ChatSignalR/ChatSignalR/Hubs/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SignalR.Hubs;
+
+namespace ChatSignalR.Hubs
+{
+    public class Chat : Hub
+    {
+        //Deve ter um metodo EnviarMensagem no JavaScript
+        public void EnviarMensagem(string mensagem)
+        {
+            mensagem = "<em>" + DateTime.Now.ToString("HH:mm:ss") + "h</em> " + mensagem;
+
+            //Chama um metodo implementado no cliente via JavaScritp
+            Clients.adicionarMensagem(mensagem);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat SignalR#
+
+	Exemplo de uso do SignalR
+
+### Para saber mais ###
+Acesse a postagem do meu blog
+http://cleytonferrari.com/
