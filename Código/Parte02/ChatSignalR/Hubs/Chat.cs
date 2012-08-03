c6e937adc03188339350673430cb62a9f5a64d26
Cleyton Ferrari <cleyton@w7br.com>
2012-08-03 10:50:32 -0400

    Organizado o Projeto e adicionado a parte 2
    Adicionado a parte 2 dos exemplos sobre signalR

---
+++ Código/Parte02/ChatSignalR/Hubs/Chat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SignalR.Hubs;
+
+namespace ChatSignalR.Hubs
+{
+    public class Chat : Hub
+    {
+        //Deve ter um metodo EnviarMensagem no JavaScript
+        public void EnviarMensagem(string mensagem, string grupo)
+        {
+            mensagem = "<em>" + DateTime.Now.ToString("HH:mm:ss") + "h [" + grupo + "]</em> " + mensagem;
+
+            //Groups.Add(Context.ConnectionId, grupo);
+
+            //Chama um metodo implementado no cliente via JavaScritp
+            Clients[grupo].adicionarMensagem(mensagem);
+
+            //Caller.adicionarMensagem("Mensagem Enviada");
+
+
+        }
+
+        public void AdicionaGrupo(string grupo)
+        {
+            Caller.adicionarMensagem("Entrou no grupo: " + grupo);
+            Groups.Add(Context.ConnectionId, grupo);
+        }
+        public void RemoveGrupo(string grupo)
+        {
+            Caller.adicionarMensagem("Saiu do grupo:" + grupo);
+            Groups.Remove(Context.ConnectionId, grupo);
+        }
+    }
+}
